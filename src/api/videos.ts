810aa0c49ed7dc32cc4124ea6f392e7b86528a9a
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-06-12 01:50:51 -0700

    add signed urls for private s3 bucket

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,9 +1,9 @@
 import { rm } from "fs/promises";
 import path from "path";
 import { getBearerToken, validateJWT } from "../auth";
-import { getVideo, updateVideo } from "../db/videos";
+import { getVideo, updateVideo, type Video } from "../db/videos";
 import { respondWithJSON } from "./json";
-import { uploadVideoToS3 } from "../s3";
+import { uploadVideoToS3, generatePresignedURL } from "../s3";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 import { type ApiConfig } from "../config";
@@ -49,8 +49,7 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const key = `${aspectRatio}/${videoId}.mp4`;
   await uploadVideoToS3(cfg, key, processedFilePath, "video/mp4");
 
-  const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${key}`;
-  video.videoURL = videoURL;
+  video.videoURL = `${key}`;
   updateVideo(cfg.db, video);
 
   await Promise.all([
@@ -132,4 +131,14 @@ export async function processVideoForFastStart(inputFilePath: string) {
   }
 
   return processedFilePath;
+}
+
+export async function dbVideoToSignedVideo(cfg: ApiConfig, video: Video) {
+  if (!video.videoURL) {
+    return video;
+  }
+
+  video.videoURL = await generatePresignedURL(cfg, video.videoURL, 5 * 60);
+
+  return video;
 }
